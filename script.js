28505c052077ee6109dc848d60fe298ac2b0060a
Jeff Almeida <jeffersonalmeida@mogidascruzes.sp.gov.br>
2025-07-17 15:45:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
-
 document.getElementById("inscricaoForm").addEventListener("submit", async function(e) {
   e.preventDefault();
+
   const nome = document.getElementById("nome").value;
   const cpf = document.getElementById("cpf").value;
   const curso = document.getElementById("curso").value;
 
-const resposta = await fetch("https://script.google.com/macros/s/AKfycbwDsMqS1kv5E_Uv4h-IQUDnsiQxJsCV8JdJoL5rdEfSls4dWurl16PVIXSIOkTxvCze/exec", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/x-www-form-urlencoded"
-  },
-  body: new URLSearchParams({ nome, cpf, curso })
-});
-
+  const resposta = await fetch("https://script.google.com/macros/s/AKfycbydDtiGiDNhmxdfKQ6WGYs7pdiun26P_otrgs2ygv29sl9cpHJLnHVCiir4H4_0Ipg/exec", {
+    method: "POST",
+    mode: "no-cors",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams({ nome, cpf, curso }),
+  });
 
   document.getElementById("mensagem").textContent = "Inscrição enviada com sucesso!";
   document.getElementById("inscricaoForm").reset();
