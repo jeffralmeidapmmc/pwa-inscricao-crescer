eead8bada078a81cbf61c9ddc8c258cf7ef0d2d0
Jeff Almeida <jeffersonalmeida@mogidascruzes.sp.gov.br>
2025-07-17 16:25:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 document.getElementById("inscricaoForm").addEventListener("submit", async function(e) {
   e.preventDefault();
 
-  const nome = document.getElementById("nome").value;
-  const cpf = document.getElementById("cpf").value;
-  const curso = document.getElementById("curso").value;
+  const dados = {
+    nome: document.getElementById("nome").value,
+    email: document.getElementById("email").value,
+    telefone: document.getElementById("telefone").value,
+    telefoneRecados: document.getElementById("telefoneRecados").value,
+    nascimento: document.getElementById("nascimento").value,
+    cpf: document.getElementById("cpf").value,
+    bairro: document.getElementById("bairro").value,
+    curso: document.getElementById("curso").value
+  };
 
-  const resposta = await fetch("https://script.google.com/macros/s/AKfycbydDtiGiDNhmxdfKQ6WGYs7pdiun26P_otrgs2ygv29sl9cpHJLnHVCiir4H4_0Ipg/exec", {
+  await fetch("https://script.google.com/macros/s/AKfycbydDtiGiDNhmxdfKQ6WGYs7pdiun26P_otrgs2ygv29sl9cpHJLnHVCiir4H4_0Ipg/exec", {
     method: "POST",
     mode: "no-cors",
     headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: new URLSearchParams({ nome, cpf, curso }),
+    body: new URLSearchParams(dados),
   });
 
   document.getElementById("mensagem").textContent = "Inscrição enviada com sucesso!";
