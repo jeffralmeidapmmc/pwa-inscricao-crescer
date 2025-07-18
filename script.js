7d1dd9bf19387e5167ecc204074500f28db19bbc
Jeff Almeida <jeffersonalmeida@mogidascruzes.sp.gov.br>
2025-07-18 09:03:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
-document.getElementById("inscricaoForm").addEventListener("submit", async function(e) {
-  e.preventDefault();
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("inscricaoForm").addEventListener("submit", async function(e) {
+    e.preventDefault();
 
-  const dados = {
-    nome: document.getElementById("nome").value,
-    email: document.getElementById("email").value,
-    telefone: document.getElementById("telefoneWhatsapp").value,
-    telefoneRecados: document.getElementById("telefoneRecado").value,
-    nascimento: document.getElementById("nascimento").value,
-    cpf: document.getElementById("cpf").value,
-    bairro: document.getElementById("bairro").value,
-    curso: document.getElementById("curso").value
-  };
+    const dados = {
+      nome: document.getElementById("nome").value,
+      email: document.getElementById("email").value,
+      telefoneWhatsapp: document.getElementById("telefoneWhatsapp").value,
+      telefoneRecado: document.getElementById("telefoneRecado").value,
+      nascimento: document.getElementById("nascimento").value,
+      cpf: document.getElementById("cpf").value,
+      bairro: document.getElementById("bairro").value,
+      curso: document.getElementById("curso").value
+    };
 
-  await fetch("https://script.google.com/macros/s/AKfycbz1cr2flYYWy3O47BBuzclguQNfVRieWYwwoGVzKEHMXG6B5BYooYFM5DkySuVmmtx2/exec", {
-    method: "POST",
-    mode: "no-cors",
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: new URLSearchParams(dados),
+    await fetch("https://script.google.com/macros/s/AKfycbz1cr2flYYWy3O47BBuzclguQNfVRieWYwwoGVzKEHMXG6B5BYooYFM5DkySuVmmtx2/exec", {
+      method: "POST",
+      mode: "no-cors",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(dados),
+    });
+
+    document.getElementById("mensagem").textContent = "Inscrição enviada com sucesso!";
+    document.getElementById("inscricaoForm").reset();
   });
-
-  document.getElementById("mensagem").textContent = "Inscrição enviada com sucesso!";
-  document.getElementById("inscricaoForm").reset();
 });
